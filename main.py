5b2e561595a90cee977ab85d27f63b46ff41efe7
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-02-26 21:56:25 +0900

    Selenium: Google Scrapping(Refactor and Deal with Exception)

--- main.py
+++ main.py
@@ -10,40 +10,41 @@ from selenium.webdriver.common.keys import Keys # Keys 사용을 위함
 options = Options()
 options.add_experimental_option("detach", True) # 자동 꺼짐 방지
 
-KEYWORD = "finalcut"
+class GoogleKeywordScreenshotter:
+    def __init__(self, keyword, screenshots_dir):
+        self.browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        self.keyword = keyword
+        self.screenshots_dir = screenshots_dir
+    def start(self):
+        self.browser.get("https://google.com")
+        search_bar = self.browser.find_element(By.CLASS_NAME, "gLFyf")
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
+        try:
+            shitty_element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "g-blk"))
+            )
+            self.browser.execute_script(
+                """
+            const shitty = arguments[0];
+            shitty.parentElement.removeChild(shitty)
+            """, 
+                shitty_element
+            )
+        except Exception:
+            pass
+        search_results = self.browser.find_elements(By.CLASS_NAME, "g")
+        for index, search_result in enumerate(search_results):
+            class_name = search_result.get_attribute("class")
+            title = search_result.find_element(By.TAG_NAME, "h3")
+            if title.size['height'] != 0 and "liYKde" not in class_name:
+                    search_result.screenshot(f"{self.screenshots_dir}/{self.keyword}x{index}.png")
+    def finish(self):
+        self.browser.quit()
 
-# open browser
-browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-
-# go to site
-browser.get("https://google.com")
-
-# find search-bar
-search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
-
-# input keyword and send key
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
-
-# wait for javascript loading
-shitty_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "g-blk")))
-
-browser.execute_script(
-    """
-const shitty = arguments[0];
-shitty.parentElement.removeChild(shitty)
-""", 
-    shitty_element
-)
-
-# find elements
-search_results = browser.find_elements(By.CLASS_NAME, "g")
-
-# find element what I want to
-for index, search_result in enumerate(search_results):
-    class_name = search_result.get_attribute("class")
-    title = search_result.find_element(By.TAG_NAME, "h3")
-    if title.size['height'] != 0 and "liYKde" not in class_name:
-            search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
-
-browser.quit()
+domain_competitors = GoogleKeywordScreenshotter("finalcut", "screenshots")
+domain_competitors.start()
+domain_competitors.finish()
+python_competitors = GoogleKeywordScreenshotter("python book", "screenshots")
+python_competitors.start()
+python_competitors.finish()
