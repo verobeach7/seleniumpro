595a16ccf8abf178d761e2beb2698b317e2cd124
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-02-26 20:54:43 +0900

    Selenium: Google Scrapping(Taking Screenshots)

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ from selenium.webdriver.common.keys import Keys # Keys 사용을 위함
 options = Options()
 options.add_experimental_option("detach", True) # 자동 꺼짐 방지
 
+KEYWORD = "finalcut"
+
+# open browser
 browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
+# go to site
 browser.get("https://google.com")
 
+# find search-bar
 search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
 
-search_bar.send_keys("hello!")
+# input keyword and send key
+search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
 
+# find elements
 search_results = browser.find_elements(By.CLASS_NAME, "g")
 
-for search_result in search_results:
-    title = search_result.find_element(By.TAG_NAME, "h3")
-    if title:
-        print(title.text)
+# find element what I want to
+for index, search_result in enumerate(search_results):
+    class_name = search_result.get_attribute("class")
+    if "g-blk" not in class_name:
+        title = search_result.find_element(By.TAG_NAME, "h3")
+        if title.size['height'] != 0 and "liYKde" not in class_name:
+            search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
+browser.quit()
