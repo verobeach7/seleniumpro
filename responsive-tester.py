f3de3cc7f0e3b1688232822d150c8be9fe785f3d
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-02-27 19:35:21 +0900

    Selenium: Responsive Tester(Scrolling Down)

--- responsive-tester.py
+++ responsive-tester.py
@@ -1,12 +1,14 @@
+from math import ceil
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options # 자동 꺼짐 방지
 from selenium.webdriver.chrome.service import Service as ChromeService
 
-
 options = Options()
 options.add_experimental_option("detach", True) # 자동 꺼짐 방지
+
+BROWSER_HEIGHT = 1415
 
 browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
@@ -15,8 +17,13 @@ browser.maximize_window()
 
 sizes = [480, 960, 1366, 1920]
 
-print(browser.get_window_size())
-
 for size in sizes:
-    browser.set_window_size(size, 1415)
+    browser.set_window_size(size, BROWSER_HEIGHT)
+    browser.execute_script("window.scrollTo(0, 0)")
     time.sleep(3)
+    scroll_size = browser.execute_script("return document.body.scrollHeight")
+    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
+    for section in range(total_sections + 1):
+        browser.execute_script(f"window.scrollTo(0, {section * BROWSER_HEIGHT})")
+        print(section)
+        time.sleep(2)
