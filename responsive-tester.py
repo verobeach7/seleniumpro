51ada7d3bacb945ee98875eec1276aea6f54bacc
verobeach7 <60215757+verobeach7@users.noreply.github.com>
2023-02-27 20:30:51 +0900

    Selenium: Responsive Tester(Refactoring)

--- responsive-tester.py
+++ responsive-tester.py
@@ -4,25 +4,45 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options # 자동 꺼짐 방지
 from selenium.webdriver.chrome.service import Service as ChromeService
+from urllib.parse import urlparse
+import re
+import os
 
 options = Options()
 options.add_experimental_option("detach", True) # 자동 꺼짐 방지
 
-BROWSER_HEIGHT = 1415
+class ResponsiveTester:
+    def __init__(self, urls):
+        self.browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        self.browser.maximize_window()
+        self.urls = urls
+        self.sizes = [480, 960, 1366, 1920]
 
-browser = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+    def screenshot(self, url, dir):
+        BROWSER_HEIGHT = self.browser.get_window_size().get("height")
+        self.browser.get(url)
+        for size in self.sizes:
+            self.browser.set_window_size(size, BROWSER_HEIGHT)
+            self.browser.execute_script("window.scrollTo(0, 0)")
+            INNER_HEIGHT = self.browser.execute_script("return window.innerHeight")
+            time.sleep(1)
+            scroll_size = self.browser.execute_script("return document.body.scrollHeight")
+            total_sections = ceil(scroll_size / INNER_HEIGHT)
+            for section in range(total_sections + 1):
+                self.browser.execute_script(f"window.scrollTo(0, {section * INNER_HEIGHT})")
+                time.sleep(0.5)
+                self.browser.save_screenshot(f"screenshots/{dir}/{dir}_{size}x{section}.png")
 
-browser.get("https://nomadcoders.co")
-browser.maximize_window()
+    def start(self):
+        for url in self.urls:
+            parsed = urlparse(url).netloc.replace("www.", "")
+            dir = re.sub(r'\.\w*', "", parsed)
+            check_path = os.path.isdir(f'./screenshots/{dir}')
+            if not check_path:
+                os.mkdir(f'./screenshots/{dir}')
+            else:
+                pass
+            self.screenshot(url, dir)
 
-sizes = [480, 960, 1366, 1920]
-
-for size in sizes:
-    browser.set_window_size(size, BROWSER_HEIGHT)
-    time.sleep(2)
-    scroll_size = browser.execute_script("return document.body.scrollHeight")
-    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
-    for section in range(total_sections):
-        browser.execute_script(f"window.scrollTo(0, {section * BROWSER_HEIGHT})")
-        time.sleep(1)
-        browser.save_screenshot(f"screenshots/{size}x{section}.png")
+tester = ResponsiveTester(["https://nomadcoders.co", "https://www.naver.com"])
+tester.start()
